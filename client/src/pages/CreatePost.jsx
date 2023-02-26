f9b489af61003893d16154584487b26b84b1c86b
Bogdan Vasilev <bogdanvasilev@MacBook-Pro-Bogdan.local>
2023-02-26 14:36:01 +0200

    change link

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
             try {
                 setGeneratingImg(true);
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/dalle",
+                    "https://dall-e-gd8z.onrender.com/api/v1/dalle",
                     {
                         method: "POST",
                         headers: {
@@ -54,7 +54,7 @@ const CreatePost = () => {
 
             try {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/post",
+                    "https://dall-e-gd8z.onrender.com/api/v1/post",
                     {
                         method: "POST",
                         headers: {
